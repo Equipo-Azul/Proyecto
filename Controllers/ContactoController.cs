9000a17c01a3c49c6a98f03581f9db2e752a2873
Julay14 <juliovilchez1239@gmail.com>
2023-04-21 22:00:43 -0500

    first commit

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AniWeb_Comics.Models;
+using AniWeb_Comics.Data;
+
+namespace AniWeb_Comics.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ContactoController(ILogger<ContactoController> logger,
+         ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "El contacto ya esta registrado";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AniWeb_Comics.Models;
+
+namespace AniWeb_Comics.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Contacto> DataContactos { get; set; }
+    public DbSet<Producto> DataProductos { get; set; }
+}
